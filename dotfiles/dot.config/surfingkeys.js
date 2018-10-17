9a6ed9e37b3817352fc75463fb08a91baec69620
thinca <thinca@gmail.com>
2018-10-17 20:09:08 +0900

    Add a config file for Surfingkeys

---
+++ dotfiles/dot.config/surfingkeys.js
@@ -0,0 +1,32 @@
+//Hints.characters = "asdfgqwertzxcvb";  // Default
+Hints.characters = "jklfdsahguiorewtymnpbvcxz";
+// Hints.numericHints = true;
+
+settings.hintAlign = "left";
+// settings.omnibarPosition = "bottom";  // Default: "middle"
+settings.focusFirstCandidate = true;
+settings.scrollStepSize = 140;  // Default: 70
+
+map("<Ctrl-p>", "E");
+map("<Ctrl-n>", "R");
+
+map("gt", "R");
+map("gT", "E");
+
+map("<", "<<");
+map(">", ">>");
+
+map("p", "cc");
+map("u", "X");
+
+map("H", "S");
+map("L", "D");
+
+map("F", "af");
+
+map("<Ctrl-u>", "e");
+map("<Ctrl-d>", "d");
+
+mapkey("b", 'Choose a tab with omnibar', () => {
+    Front.openOmnibar({type: "Tabs"});
+});
