5c1da24b54b2bb1ddd404ca161604b55b53de7f6
thinca <thinca@gmail.com>
2020-02-04 18:37:31 +0900

    zsh: Remove setting $LUA_PATH and $LUA_CPATH
    These are set by `luarocks path`.

--- dotfiles/dot.zsh/scripts/lua.zsh
+++ dotfiles/dot.zsh/scripts/lua.zsh
@@ -2,5 +2,3 @@
 if which luarocks &>/dev/null; then
 	eval $(luarocks path)
 fi
-export LUA_PATH="$LUA_PATH;$HOME/.luarocks/share/lua/5.1/?.lua"
-export LUA_CPATH="$LUA_PATH;$HOME/.luarocks/lib/lua/5.1/?.so"
