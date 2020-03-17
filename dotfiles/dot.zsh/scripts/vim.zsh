da72496d4907aaf384255fe3cc448d29adeaacbc
thinca <thinca@gmail.com>
2020-03-17 11:34:19 +0900

    vim: Goodbye, Lingr

--- dotfiles/dot.zsh/scripts/vim.zsh
+++ dotfiles/dot.zsh/scripts/vim.zsh
@@ -3,6 +3,4 @@ alias v="DISPLAY= vim"
 # Plain vim
 alias pvim="vim -u NONE -i NONE -n -N -n"
 
-alias lingr="DISPLAY= vim --cmd 'let g:lingr = 1' +LingrLaunch"
-
 export VOLTPATH=$HOME/.local/share/vim/volt
