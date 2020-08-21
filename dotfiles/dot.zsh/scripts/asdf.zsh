cec8c80aad6fbf0aa1e14c746dd336e418346a9d
thinca <thinca@gmail.com>
2020-08-21 19:39:15 +0900

    asdf: Skip auto reshim
    This is so slow.

--- dotfiles/dot.zsh/scripts/asdf.zsh
+++ dotfiles/dot.zsh/scripts/asdf.zsh
@@ -7,3 +7,5 @@ export PYTHON_CONFIGURE_OPTS="--enable-shared"
 # Need patch to asdf-perl
 export PERL_CONFIGURE_OPTS="-Duseshrplib"
 export ASDF_RACKET_CONFIG_FLAGS="--disable-futures --disable-places --disable-gracket --disable-docs"
+
+export ASDF_SKIP_RESHIM=1
