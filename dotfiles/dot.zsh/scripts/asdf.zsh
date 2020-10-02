83ef86e4f3028720101b9ad8f10eb09a2deca1d2
thinca <thinca@gmail.com>
2020-10-02 19:47:21 +0900

    asdf: Enable dynlib for Racket

--- dotfiles/dot.zsh/scripts/asdf.zsh
+++ dotfiles/dot.zsh/scripts/asdf.zsh
@@ -6,6 +6,6 @@ export RUBY_CONFIGURE_OPTS="--enable-shared"
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
 # Need patch to asdf-perl
 export PERL_CONFIGURE_OPTS="-Duseshrplib"
-export ASDF_RACKET_CONFIG_FLAGS="--disable-futures --disable-places --disable-gracket --disable-docs"
+export ASDF_RACKET_CONFIG_FLAGS="--enable-dynlib --disable-futures --disable-places --disable-gracket --disable-docs"
 
 export ASDF_SKIP_RESHIM=1
