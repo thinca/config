e612eb6458eb5749587047b78bfd9297adabf8b2
thinca <thinca@gmail.com>
2020-10-14 02:39:58 +0900

    asdf: Do not enable dynlib for racket on Mac
    It is not supported.
    https://github.com/racket/racket/blob/366bdcb9e72de794ac1847b11302b600fc888bc0/racket/src/README.txt#L291-L294

--- dotfiles/dot.zsh/scripts/asdf.zsh
+++ dotfiles/dot.zsh/scripts/asdf.zsh
@@ -6,6 +6,9 @@ export RUBY_CONFIGURE_OPTS="--enable-shared"
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
 # Need patch to asdf-perl
 export PERL_CONFIGURE_OPTS="-Duseshrplib"
-export ASDF_RACKET_CONFIG_FLAGS="--enable-dynlib --disable-futures --disable-places --disable-gracket --disable-docs"
+export ASDF_RACKET_CONFIG_FLAGS="--disable-futures --disable-places --disable-gracket --disable-docs"
+if [[ "${SYSTEM_NAME}" != "mac" ]]; then
+	ASDF_RACKET_CONFIG_FLAGS="--enable-dynlib ${ASDF_RACKET_CONFIG_FLAGS}"
+fi
 
 export ASDF_SKIP_RESHIM=1
