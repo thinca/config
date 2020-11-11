5f27b160a9b1e41af5d7fa809308f64df1043146
thinca <thinca@gmail.com>
2020-11-12 01:00:57 +0900

    p10k: Use ISO 8601 for time format

--- dotfiles/dot.zsh/p10k.zsh
+++ dotfiles/dot.zsh/p10k.zsh
@@ -1483,7 +1483,7 @@
   # Current time color.
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
   # Format for the current time: 09:51:02. See `man 3 strftime`.
-  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
+  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%FT%T}'
   # If set to true, time will update when you hit enter. This way prompts for the past
   # commands will contain the start times of their commands as opposed to the default
   # behavior where they contain the end times of their preceding commands.
