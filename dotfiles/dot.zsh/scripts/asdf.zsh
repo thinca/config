06232dbbcedabaa98985e550d4a242012bcf162f
thinca <thinca@gmail.com>
2021-02-22 03:20:08 +0900

    asdf: Use Racket BC to enable +mzscheme in Vim
    Racket CS is the default from Racket 8.0,
    but Vim does not support Racket CS.

--- dotfiles/dot.zsh/scripts/asdf.zsh
+++ dotfiles/dot.zsh/scripts/asdf.zsh
@@ -6,7 +6,7 @@ export RUBY_CONFIGURE_OPTS="--enable-shared"
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
 # Need patch to asdf-perl
 export PERL_CONFIGURE_OPTS="-Duseshrplib"
-export ASDF_RACKET_CONFIG_FLAGS="--disable-futures --disable-places --disable-gracket --disable-docs"
+export ASDF_RACKET_CONFIG_FLAGS="--enable-bcdefault --disable-futures --disable-places --disable-gracket --disable-docs"
 if [[ "${SYSTEM_NAME}" != "mac" ]]; then
 	ASDF_RACKET_CONFIG_FLAGS="--enable-dynlib ${ASDF_RACKET_CONFIG_FLAGS}"
 fi
